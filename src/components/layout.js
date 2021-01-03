2d4bc75e537261cf9338c8f97ab9db60e50ef8fd
jesben <jesben.sevillano.h@uni.pe>
2021-01-03 15:09:41 -0500

    refactoring code

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,19 +1,14 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import { Row, Col } from "reactstrap"
 
 import Header from "./header"
+import Sidebar from "./Sidebar"
 import Footer from "../components/Footer"
 import "../styles/index.scss"
 
-const Layout = ({ children }) => {
+const Layout = ({ children, pageTitle }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -32,7 +27,14 @@ const Layout = ({ children }) => {
       ></script>
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
       <div className="container" id="content">
-        <main>{children}</main>
+        <h1>{pageTitle}</h1>
+        <Row>
+          <Col md="8">{children}</Col>
+          <Col md="4">
+            {" "}
+            <Sidebar />{" "}
+          </Col>
+        </Row>
         <Footer></Footer>
       </div>
     </>
--- src/pages/team.js
+++ src/pages/team.js
@@ -4,9 +4,59 @@ import SEO from "../components/seo"
 
 const TeamPage = () => {
   return (
-    <Layout>
+    <Layout pageTitle="Our team">
       <SEO title="Team" keywords={[`gatsby`, `application`, `reac`]} />
-      <h1>Team page</h1>
+      <p>
+        Voluptate beatae nostrum nemo quisquam earum impedit esse perspiciatis
+        quaerat reprehenderit nesciunt ea officia hic consectetur, doloribus
+        ratione! Esse eius quam et est tenetur ducimus. Fugiat. Est pariatur
+        earum sint cum nemo minima a magni maxime aliquam ipsa facilis deserunt
+        totam quas expedita labore, iusto suscipit quo consequatur iste maiores
+        aut nobis. Nemo minima incidunt officiis! Cumque, fugit odit, laborum
+        earum omnis eligendi qui delectus impedit enim ipsam commodi. Tempore
+        consequatur corporis odit architecto temporibus similique porro tempora
+        magni iste ea quibusdam, ex at ratione sed. Dolores atque assumenda amet
+        itaque natus ullam rerum quis voluptatem enim accusantium totam saepe
+        provident autem, eos, nulla fugiat sint nam non accusamus soluta
+        sapiente vero, earum maxime corporis? Libero! Quae repudiandae placeat
+        dolorum vel officiis praesentium quibusdam, fugiat consectetur sequi
+        optio aliquam consequatur expedita numquam alias nesciunt voluptatem
+        assumenda sunt odio blanditiis at quos rem cupiditate, vitae iure. Sint.
+        Veritatis mollitia optio sint necessitatibus? Tenetur incidunt, laborum
+        cupiditate atque aperiam dolore, quam illum magni amet aliquam maiores
+        eaque beatae quis odit repellendus similique? Sed eaque tempore
+        architecto pariatur doloremque? At asperiores quia ducimus adipisci,
+        laboriosam perspiciatis nihil, quasi neque veniam dolore assumenda ipsam
+        eum? Sed eveniet dolore maxime soluta atque. Ipsum nam praesentium
+        placeat ex aut, itaque sint maxime! Enim nemo veniam recusandae placeat
+        possimus minus explicabo libero. Quis quisquam at, iste alias quaerat
+        consectetur mollitia. Illo totam, quo officiis mollitia provident
+        voluptates magni quasi accusantium quibusdam. Nostrum, minus. Tempora
+        mollitia est exercitationem doloribus, possimus numquam saepe
+        repudiandae itaque laborum eligendi nulla dolor quasi facilis
+        repellendus eos accusamus esse velit laudantium! Tempora eaque rerum
+        veniam aut cumque sit quidem. Enim dicta laborum rem impedit facere
+        nobis dolorem sint exercitationem corporis neque dolores repellat illum
+        laboriosam maxime iure aliquid aperiam provident, voluptate similique ea
+        doloremque qui, ad suscipit! Mollitia, molestias. Quisquam veritatis
+        explicabo omnis, repellat reiciendis sequi accusantium est tempora
+        recusandae placeat nesciunt magni tenetur consectetur at sapiente sint
+        labore. Magnam pariatur eveniet nisi debitis ut aspernatur veniam.
+        Molestiae, eum? Quos laboriosam distinctio, odio eos odit facere, maxime
+        porro, quidem ex eius deleniti eaque? Neque, officiis sunt natus quis
+        veniam itaque, blanditiis porro, temporibus autem optio nulla beatae hic
+        perferendis. Ex provident similique quidem id itaque. Necessitatibus
+        saepe nihil ratione placeat provident accusamus, iure illum maxime cum
+        blanditiis quibusdam aperiam quasi ut omnis mollitia illo nisi
+        aspernatur dicta laborum odit. Cum aperiam facere voluptatum
+        voluptatibus esse natus libero nesciunt illo? Velit quaerat dolor
+        obcaecati corporis delectus nemo, debitis ipsa nobis architecto expedita
+        quo aspernatur consectetur ipsam iusto, praesentium perspiciatis
+        reprehenderit? Enim, cupiditate, dignissimos quam ab cum voluptatibus
+        officia amet officiis placeat expedita sint explicabo dolorum cumque
+        excepturi facere ducimus. Minus id, magnam atque autem corporis omnis
+        distinctio animi quos corrupti.
+      </p>
     </Layout>
   )
 }
